ad219bd3d6d18534e0c594c674b056f98e2f38cb
Ted Gueniche <ted.gueniche@gmail.com>
2016-02-21 22:22:33 -0400

    Fixed date in YahooAPI for (day < 10)

--- YahooAPI.js
+++ YahooAPI.js
@@ -64,6 +64,7 @@ YahooAPI.getQuote = function(symbol, date, success, error) {
 YahooAPI.getDateString = function(date) {
 
 	var day = date.getDate();
+	day = (day < 10) ? "0" + day : day;
 	var month = date.getMonth() + 1;
 	month = (month < 10) ? "0" + month : month;
 	var year = date.getFullYear();
